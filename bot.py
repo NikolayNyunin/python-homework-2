1e8935a5f69071d8795b43de8e5534123582e91e
Nikolay Nyunin <nikolay.nyunin@gmail.com>
2025-01-21 13:32:01 +0300

    Start implementing a Telegram bot

---
+++ bot.py
@@ -0,0 +1,25 @@
+from config import BOT_TOKEN
+
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+
+import asyncio
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message()
+async def echo(message: Message):
+    await message.reply(f'Получено сообщение: "{message.text}"')
+
+
+async def main():
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
