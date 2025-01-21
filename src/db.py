82ceef8075320f4233dd70d44cb7e873d56728a5
Nikolay Nyunin <nikolay.nyunin@gmail.com>
2025-01-21 22:15:14 +0300

    Expand the database interaction model

--- src/db.py
+++ src/db.py
@@ -38,11 +38,55 @@ async def update_data(user_id: int) -> None:
         raise KeyError(f'Пользователь с id {user_id} не найден в БД')
 
     data = user_data[user_id]
-    data['water_target'] = await calculate_water_target(data['weight'], data['city'])
     if data['date'] != date.today():
+        data['water_target'] = await calculate_water_target(data['weight'], data['city'])
         data['logged_water'] = 0
         data['logged_calories'] = 0
         data['burned_calories'] = 0
         data['date'] = date.today()
 
     logger.info(f'Данные пользователя {user_id} обновлены')
+
+
+async def log_water(user_id: int, water_volume: int) -> None:
+    """Учёт выпитой воды (в мл)."""
+
+    await update_data(user_id)
+    user_data[user_id]['logged_water'] += water_volume
+
+    logger.info(f'Пользователь {user_id}: {water_volume} мл воды выпито')
+
+
+async def log_consumed_calories(user_id: int, calories_count: int) -> None:
+    """Учёт потреблённых калорий (в ккал)."""
+
+    await update_data(user_id)
+    user_data[user_id]['logged_calories'] += calories_count
+
+    logger.info(f'Пользователь {user_id}: {calories_count} ккал употреблено')
+
+
+async def log_burned_calories(user_id: int, calories_count: int) -> None:
+    """Учёт сожжённых калорий (в ккал)."""
+
+    await update_data(user_id)
+    user_data[user_id]['burned_calories'] += calories_count
+
+    logger.info(f'Пользователь {user_id}: {calories_count} ккал сожжено')
+
+
+async def get_progress(user_id: int) -> dict:
+    """Получение прогресса в выполнении дневных целей."""
+
+    await update_data(user_id)
+    data = user_data[user_id]
+
+    logger.info(f'Прогресс пользователя {user_id} получен')
+
+    return {
+        'water_target': data['water_target'],
+        'calorie_target': data['calorie_target'],
+        'logged_water': data['logged_water'],
+        'logged_calories': data['logged_calories'],
+        'burned_calories': data['burned_calories']
+    }
